eb3b723e402ce18358610d0fd5540c1ee91d5ae8
saajida mathen <mathensaajida@gmail.com>
2023-06-12 14:57:59 +0200

    dice roll game works

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-let currentPlayer = 1;
+let currentPlayer = 0;
+let gameStarted = false;
+let player1Wins = 0;
+let player2Wins = 0;
+let numRounds = 1;
 
 function rollDice() {
   const randomNum = Math.floor(Math.random() * 6) + 1;
   const diceImage = 'images/dice' + randomNum + '.png';
-  document.querySelectorAll('img')[currentPlayer - 1].setAttribute('src', diceImage);
+  document.querySelectorAll('img')[currentPlayer].setAttribute('src', diceImage);
 
-  if (currentPlayer === 1) {
+  if (currentPlayer === 0) {
     document.querySelector('h1').innerHTML = 'Player 1 rolled a ' + randomNum;
-    currentPlayer = 2;
+    currentPlayer = 1;
     button.innerHTML = 'Player 2 Roll Dice';
+    button.style.backgroundColor = 'blue'; // change button color to blue for player 2
   } else {
     document.querySelector('h1').innerHTML = 'Player 2 rolled a ' + randomNum;
-    currentPlayer = 1;
+    currentPlayer = 0;
     button.innerHTML = 'Player 1 Roll Dice';
+    button.style.backgroundColor = 'red'; // change button color to red for player 1
     determineWinner();
   }
 }
@@ -22,11 +28,47 @@ function determineWinner() {
   const secondDiceNum = parseInt(document.querySelectorAll('img')[1].getAttribute('src').slice(-5, -4));
 
   if (firstDiceNum > secondDiceNum) {
-    document.querySelector('h1').innerHTML = 'Player 1 wins!';
+    document.querySelector('h1').innerHTML = 'Player 1 wins round ' + numRounds + '!';
+    player1Wins++;
   } else if (firstDiceNum < secondDiceNum) {
-    document.querySelector('h1').innerHTML = 'Player 2 wins!';
+    document.querySelector('h1').innerHTML = 'Player 2 wins round ' + numRounds + '!';
+    player2Wins++;
   } else {
-    document.querySelector('h1').innerHTML = 'Draw!';
+    document.querySelector('h1').innerHTML = 'Round ' + numRounds + ' is a draw!';
+  }
+
+  if (player1Wins === 2) {
+    document.querySelector('h1').innerHTML = 'Player 1 wins the game!';
+    button.style.display = 'none';
+    const newGameButton = document.createElement('button');
+    newGameButton.innerHTML = 'Start New Game';
+    newGameButton.addEventListener('click', () => {
+      newGameButton.style.display = 'none';
+      startButton.style.display = 'block';
+      button.style.display = 'none';
+      currentPlayer = 0;
+      player1Wins = 0;
+      player2Wins = 0;
+      numRounds = 1;
+    });
+    document.body.appendChild(newGameButton);
+  } else if (player2Wins === 2) {
+    document.querySelector('h1').innerHTML = 'Player 2 wins the game!';
+    button.style.display = 'none';
+    const newGameButton = document.createElement('button');
+    newGameButton.innerHTML = 'Start New Game';
+    newGameButton.addEventListener('click', () => {
+      newGameButton.style.display = 'none';
+      startButton.style.display = 'block';
+      button.style.display = 'none';
+      currentPlayer = 0;
+      player1Wins = 0;
+      player2Wins = 0;
+      numRounds = 1;
+    });
+    document.body.appendChild(newGameButton);
+  } else {
+    numRounds++;
   }
 }
 
@@ -35,5 +77,61 @@ button.addEventListener("click", () => {
   rollDice();
 });
 
-rollDice(); // initial roll on page load
+const newGameButton = document.createElement('button');
+newGameButton.innerHTML = 'Start New Game';
+newGameButton.style.display = 'none';
+newGameButton.addEventListener('click', () => {
+  location.reload(); // refresh the page
+});
 
+document.body.appendChild(newGameButton);
+
+function startGame() {
+  gameStarted = true;
+  startButton.style.display = "none";
+  button.style.display = "block";
+  currentPlayer = 0;
+  player1Wins = 0;
+  player2Wins = 0;
+  numRounds = 1;
+  document.querySelector('h1').innerHTML = 'Player 1 Roll Dice'; // update the heading
+  document.querySelector('img').setAttribute('src', 'images/dice1.png'); // reset the dice images
+  document.querySelectorAll('img')[1].setAttribute('src', 'images/dice1.png');
+}
+
+function determineWinner() {
+  const firstDiceNum = parseInt(document.querySelectorAll('img')[0].getAttribute('src').slice(-5, -4));
+  const secondDiceNum = parseInt(document.querySelectorAll('img')[1].getAttribute('src').slice(-5, -4));
+
+  if (firstDiceNum > secondDiceNum) {
+    document.querySelector('h1').innerHTML = 'Player 1 wins round ' + numRounds + '!';
+    player1Wins++;
+  } else if (firstDiceNum < secondDiceNum) {
+    document.querySelector('h1').innerHTML = 'Player 2 wins round ' + numRounds + '!';
+    player2Wins++;
+  } else {
+    document.querySelector('h1').innerHTML = 'Round ' + numRounds + ' is a draw!';
+  }
+
+  if (player1Wins === 2) {
+    document.querySelector('h1').innerHTML = 'Player 1 wins the game!';
+    button.style.display = 'none';
+    newGameButton.style.display = 'block'; // show the button
+  } else if (player2Wins === 2) {
+    document.querySelector('h1').innerHTML = 'Player 2 wins the game!';
+    button.style.display = 'none';
+    newGameButton.style.display = 'block'; // show the button
+  } else {
+    numRounds++;
+  }
+}
+
+startButton.addEventListener("click", () => {
+  startGame();
+});
+
+button.addEventListener("click", () => {
+  if (gameStarted) {
+    rollDice();
+  }
+});
