51de6931f49539cf16263732246fe15b37411ac1
saajida mathen <mathensaajida@gmail.com>
2023-06-12 17:00:22 +0200

    dice roll game works

--- index.js
+++ index.js
@@ -135,3 +135,20 @@ button.addEventListener("click", () => {
     rollDice();
   }
 });
+
+let cube = document.getElementById('cube');
+
+let min = 1;
+let max = 24;
+
+cube.onclick = function() {
+  let xRand = getRandom(max, min);
+  let yRand = getRandom(max, min);
+
+  cube.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+  cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+}
+
+function getRandom(max, min) {
+  return (Math.floor(Math.random() * (max-min)) + min) * 90;
+}
